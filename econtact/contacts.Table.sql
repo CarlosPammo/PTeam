d51c958c4e77929a1246aa878132ff6f33e4b835
omar-chavez <omar-fcr@outlook.com>
2017-06-22 21:19:16 -0400

    subiendo tabla

---
+++ econtact/contacts.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] TEXT NOT NULL, 
+    [lastname] TEXT NOT NULL, 
+    [telephone] TINYINT NOT NULL, 
+    [celephone] TINYINT NOT NULL, 
+    [address] TEXT NOT NULL, 
+    [group] TEXT NOT NULL, 
+    [birthdate] DATETIME NOT NULL
+)
